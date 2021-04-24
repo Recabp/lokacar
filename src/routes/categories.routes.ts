bafe78378348dabe3c141e5fc242dce59c618b42
recabp <renatocabp@outlook.com>
2021-04-24 14:36:55 -0300

    first commit

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,16 +1,20 @@
 import { Router } from "express";
+import { v4 as uuidV4 } from "uuid";
 
 const categoriesRoutes = Router();
 
 const catefories = [];
 
-categoriesRoutes.post("/categories", (request, response) => {
+categoriesRoutes.post("", (request, response) => {
   const { name, description } = request.body;
 
-  catefories.push({
+  const category = {
     name,
     description,
-  });
+    id: uuidV4(),
+  };
+
+  catefories.push(category);
 
   return response.status(201).send();
 });
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,6 @@ import categoriesRoutes from "./routes/categories.routes";
 
 const app = express();
 
-app.use(categoriesRoutes);
+app.use("/categories", categoriesRoutes);
 
 app.listen(6666, () => console.log("server is running on port 6666!"));
