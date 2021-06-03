3d9ae94badc80edffaf2f613227e5732cb1d2d45
recabp <renatocabp@outlook.com>
2021-06-02 22:08:28 -0300

    routes refatorated

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,8 @@
 import express from 'express';
-import specificationsRoutes from './routes/specifications.routes';
-import categoriesRoutes from './routes/categories.routes';
+import router from './routes';
 
 const app = express();
 
-app.use('/categories', categoriesRoutes);
-app.use('/specifications', specificationsRoutes);
+app.use(router);
 
 app.listen(6666, () => console.log('server is running on port 6666!'));
